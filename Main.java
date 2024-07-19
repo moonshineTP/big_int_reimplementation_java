ef305714bd164f7294a96f4de23507437a47256e
MoonshineTP <giahung260305@gmail.com>
2024-07-19 14:37:41 +0700

    add Main

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import mainPack.bigNum;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner TP = new Scanner(System.in);
+        String a = TP.nextLine();
+        String b = TP.nextLine();
+        bigNum u = new bigNum();
+        u.setALL(a);
+        bigNum v = new bigNum();
+        v.setALL(b);
+        long startTime = System.nanoTime();
+        System.out.println(bigNum.product(u, v).getNUM());
+        long endTime = System.nanoTime();
+        System.out.printf("Time taken: %.12f seconds", (endTime - startTime) / 1000000000f); 
+        TP.close();
+    }
+}
